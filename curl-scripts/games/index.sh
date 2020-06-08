289b180605a262e8051fabe8a50d04bbc3d2d50d
Andrew-Chun <i.am.andrewchun@gmail.com>
2020-06-08 12:35:56 -0500

    index.sh curl script gets all games won by user

--- curl-scripts/games/index.sh
+++ curl-scripts/games/index.sh
@@ -1,7 +1,7 @@
 API="https://tic-tac-toe-wdi.herokuapp.com"
 URL_PATH="/games"
 
-curl "${API}${URL_PATH}" \
+curl "${API}${URL_PATH}?over=true" \
   --include \
   --request GET \
   --header "Authorization: Bearer ${TOKEN}"
